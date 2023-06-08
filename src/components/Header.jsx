9c1f7fab923c064eb8dbcc594cc0a6e18b15c620
WaiLinnAung16 <waia00198@gmail.com>
2023-06-08 15:18:45 +0630

    start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <div>
+        <div>
+          <div>
+            <p>icon</p>
+            <p>1-634-567-34</p>
+          </div>
+          <div>
+            <p>icon</p>
+            <p>Book@Hotale.co</p>
+          </div>
+        </div>
+        <div>
+          <p>Login</p>
+          <p>Sign Up</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
