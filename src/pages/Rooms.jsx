acc5cbb2edf6223402040d25fcb0bef966dd2cc7
Min1870 <minkhantzaw.peti@gmail.com>
2023-06-10 13:34:08 +0630

    roomCardUpdate

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,50 +1,21 @@
 import React from "react";
 import BannerForAll from "../utils/BannerForAll";
-import luxury from "../assets/rooms/LuxurySuite.jpg";
-import { Link } from "react-router-dom";
-import { IoBedOutline } from "react-icons/io5";
-import { MdPeopleOutline, MdKeyboardArrowRight } from "react-icons/md";
+import RoomCard from "../utils/RoomCard";
 
 const Rooms = () => {
   return (
     <div>
       <BannerForAll text={"Rooms"} />
-      <div className="flex flex-wrap mt-10 items-center justify-center gap-5">
-        <div className="max-w-sm bg-white border border-gray-100 rounded-[20px] shadow ">
-          <Link to={""} className="relative">
-            <img className="rounded-t-[20px]" src={luxury} alt="" />
-            <div className=" absolute  px-[20px] py-[21px] text-[20px] rounded-[10px] bg-black bottom-[10px] left-[25px] font-semibold">
-              <span className=" text-white mr-[5px] ">From</span>
-              <span className=" text-white">$90</span>
-              <span className=" text-white text-[15px] font-normal"> / night</span>
-            </div>
-          </Link>
-          <div className="px-[40px] py-[45px]">
-            <Link to={""}>
-              <h5 className="mb-[20px] text-[30px] font-medium transition duration-100 hover:text-[#656565]">
-                Luxury Suite
-              </h5>
-            </Link>
-            <div class="mb-[30px] font-normal flex gap-4 flex-col md:flex-row">
-              <div className=" flex items-center gap-4">
-                <IoBedOutline className=" text-[#848484] text-[28px]" />
-                <span className="text-[#656565]">1 King Bed</span>
-              </div>
-              <div className=" flex items-center gap-4">
-                <MdPeopleOutline className=" text-[#848484] text-[28px]" />
-                <span className="text-[#656565]">4 Guests</span>
-              </div>
-            </div>
-            <Link
-              to={""}
-              className="inline-flex mb-[30px] items-center py-2 text-sm font-medium text-center text-black tracking-widest uppercase focus:ring-4 focus:outline-none"
-            >
-              Book Now
-              <MdKeyboardArrowRight />
-            </Link>
-          </div>
-        </div>
-        
+      <div className="flex flex-wrap mt-20 items-center justify-center gap-10">
+        <RoomCard/>
+        <RoomCard/>
+        <RoomCard/>
+        <RoomCard/>
+        <RoomCard/>
+        <RoomCard/>
+        <RoomCard/>
+        <RoomCard/>
+        <RoomCard/>
       </div>
     </div>
   );
