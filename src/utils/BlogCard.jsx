c3a7af779fbe896b6a77112722ac3115b2f1786f
Min1870 <minkhantzaw.peti@gmail.com>
2023-06-11 21:59:06 +0630

    UblogC&roomC

--- src/utils/BlogCard.jsx
+++ src/utils/BlogCard.jsx
@@ -1,14 +1,14 @@
 import { MdArrowRightAlt } from "react-icons/md";
 import { Link } from "react-router-dom";
 
-const BlogCard = () => {
+const BlogCard = ({ blog }) => {
   return (
     <div className="max-w-sm bg-white border border-gray-100 shadow ">
       <Link to={""} className="relative">
         <div className=" inline-block border-2 overflow-hidden relative group">
           <img
             className="group-hover:scale-110 transition-all duration-300"
-            src="https://a6e8z9v6.stackpathcdn.com/hotale/resort/wp-content/uploads/sites/2/2020/10/kelsey-roenau-608583-unsplash-900x500.jpg"
+            src={blog?.img}
             alt=""
           />
           <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-0 group-hover:bg-opacity-30 transition duration-300"></div>
@@ -16,20 +16,23 @@ const BlogCard = () => {
       </Link>
       <div className="pl-[40px] pr-[45px] pt-[35px] pb-[20px]">
         <Link to={""}>
-          <span className=" text-[#4c4c4c] text-[13px] font-bold">
+          <div className=" text-[#4c4c4c] text-[13px] font-bold mb-[4px]">
             Dec 17, 2021
-          </span>
-          <h5 className="mb-[20px] text-[30px] font-medium transition duration-200 hover:text-[#656565]">
-            Cities To Visit For Your First Time In Europe
-          </h5>
+          </div>
+          <div>
+            <h3 className="mb-[20px] text-[27px] text-[#262626] font-bold transition duration-200 hover:text-[#656565]">
+              {blog?.title}
+            </h3>
+          </div>
         </Link>
-        <p className=" text-[#656565] mb-5 text-[17px]">
-          Far far away, behind the word mountains, far from the countries
-          Vokalia and Consonantia, there live the blind...
-        </p>
+        <div>
+          <p className=" text-[#656565] mb-5 text-[17px]">
+            {blog?.desc.substring(0, 109)}...
+          </p>
+        </div>
         <Link
           to={""}
-          className="inline-flex gap-3 mb-[30px] items-center py-2 text-sm font-medium text-center text-black tracking-widest uppercase focus:ring-4 focus:outline-none"
+          className=" hover-underline-animation_blog gap-3 mb-[50px] text-[17px]  items-center py-2 font-normal transition duration-200 hover:text-[#656565] text-[#212121] focus:ring-4 focus:outline-none"
         >
           Read More
           <MdArrowRightAlt size={20} />
