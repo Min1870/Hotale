942b6de92305d0fd7a736788935c4bec670b9774
Min1870 <minkhantzaw.peti@gmail.com>
2023-06-16 18:07:32 +0630

    homeBannerFix

--- src/utils/Banner.jsx
+++ src/utils/Banner.jsx
@@ -14,7 +14,6 @@ import SignupModal from "../components/SignupModal";
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
 
-
 const Banner = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [offsetLeft, setOffsetLeft] = useState(0);
@@ -52,7 +51,7 @@ const Banner = () => {
         )}
       </AnimatePresence>
       {/* Mobile Navbar */}
-      <div className="lg:hidden flex min-w-full items-center justify-between py-5 px-5 xl:px-[120px] text-white bg-black z-50 absolute top-0 left-0">
+      <div className="lg:hidden flex max-w-[1320px] items-center justify-between py-5 px-5 xl:px-[120px] text-white bg-black z-50 absolute top-0 left-0">
         <Link to="/">
           <img src={logo} alt="" className="w-[147px] h-[37px]" />
         </Link>
@@ -148,157 +147,170 @@ const Banner = () => {
           </NavLink>
         </div>
       </div>
-      <div className="relative h-[130vh] lg:h-[136vh] mt-24 lg:m-0">
+      <div className="relative h-[120vh] md:h-[100vh] mt-24 lg:m-0">
         {/* Laptop Navbar */}
         <div className=" relative top-0 bg-homeBg  w-full h-full bg-center bg-cover bg-no-repeat rounded-[20px]">
-          <div className="hidden lg:flex min-w-full items-center justify-between absolute z-50 text-black px-[40px] xl:px-[90px] top-8">
-            <div className=" text-white flex items-center gap-5">
-              <div className="flex items-center gap-2 text-[15px]">
-                <BsTelephone />
-                <span>1-634-567-34</span>
-              </div>
-              <div className="flex items-center gap-2 text-[15px]">
-                <HiOutlineMail size={15} />
-                <span>Book@Hotale.co</span>
-              </div>
-            </div>
-            <div className=" text-white flex gap-5">
-              <button
-                onClick={() => setOpenLoginModal((pre) => !pre)}
-                className=" bg-transparent border-r-2 px-4 font-medium"
-              >
-                Login
-              </button>
-              <button
-                className="font-medium"
-                onClick={() => setOpenSignUpModal((pre) => !pre)}
-              >
-                Sign Up
-              </button>
-            </div>
-          </div>
-
-          <div className="hidden lg:flex min-w-full items-center absolute z-50 text-black px-[40px] xl:px-[90px] justify-between top-20">
-            <Link to="/">
-              <img src={logo} alt="" className="w-[147px] h-[37px] mb-3" />
-            </Link>
-
-            <div
-              onMouseLeave={() => setOffsetLeft(0)}
-              className=" relative flex gap-10 text-white tracking-widest text-[13px] font-semibold "
-            >
-              <div
-                style={{ transform: `translateX(${offsetLeft}px)` }}
-                className=" absolute mt-[30px] transition duration-300 w-[18px] h-[2px] bg-white"
-              ></div>
-              <NavLink
-                to={"/"}
-                className=" pb-1"
-                onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
-              >
-                HOME
-              </NavLink>
-              <div className="">
-                <NavLink
-                  className=" pb-6"
-                  to={`/`}
-                  onMouseEnter={(e) => {
-                    setIsDropdownOpen(true);
-                    setOffsetLeft(e.target.offsetLeft);
-                  }}
-                  onMouseLeave={() => setIsDropdownOpen(false)}
-                >
-                  PAGES
-                </NavLink>
-                {/* dropdown  */}
-                <div
-                  onMouseEnter={() => {
-                    setIsDropdownOpen(true);
-                  }}
-                  onMouseLeave={() => {
-                    setTimeout(() => {
-                      setIsDropdownOpen(false);
-                    }, 500);
-                  }}
-                  className={`${
-                    isDropdownOpen ? "block" : "hidden"
-                  } cursor-default absolute top-10 transition duration-500 bg-white w-52 shadow py-5 px-4 flex flex-col space-y-5 z-[100]`}
-                >
-                  <NavLink
-                    to={"/about-us"}
-                    className="text-[#757575] font-normal text-[16px] transition-all hover:text-black"
-                  >
-                    About Us
-                  </NavLink>
-                  <NavLink
-                    to={"/our-team"}
-                    className="text-[#757575] font-normal text-[16px] transition-all hover:text-black"
-                  >
-                    Our Team
-                  </NavLink>
-                  <NavLink
-                    to={"/hotel-review"}
-                    className="text-[#757575] font-normal text-[16px] transition-all hover:text-black"
-                  >
-                    Hotel Reviews
-                  </NavLink>
+          <div className=" relative max-w-[1300px] mx-auto">
+            <div className="hidden lg:flex min-w-full items-center justify-between absolute z-50 text-black px-[40px] xl:px-[90px] top-8">
+              <div className=" text-white flex items-center gap-5">
+                <div className="flex items-center gap-2 text-[15px]">
+                  <BsTelephone />
+                  <span>1-634-567-34</span>
+                </div>
+                <div className="flex items-center gap-2 text-[15px]">
+                  <HiOutlineMail size={15} />
+                  <span>Book@Hotale.co</span>
                 </div>
               </div>
-              <NavLink
-                to={"/rooms"}
-                className=" pb-1"
-                onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
-              >
-                ROOMS
-              </NavLink>
-              <NavLink
-                to={"/reservation"}
-                className=" pb-1"
-                onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
-              >
-                RESERVATION
-              </NavLink>
-              <NavLink
-                to={"/blogs"}
-                className=" pb-1"
-                onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
-              >
-                BLOG
-              </NavLink>
-              <NavLink
-                to={"/contact"}
-                className=" pb-1"
-                onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
-              >
-                CONTACT
-              </NavLink>
-            </div>
-            <div className="flex items-center gap-8 text-white">
-              <div className=" flex items-center gap-2">
-                <div className="">USD</div>
-                <IoMdArrowDropdown />
+              <div className=" text-white flex gap-5">
+                <button
+                  onClick={() => setOpenLoginModal((pre) => !pre)}
+                  className=" bg-transparent border-r-2 px-4 font-medium"
+                >
+                  Login
+                </button>
+                <button
+                  className="font-medium"
+                  onClick={() => setOpenSignUpModal((pre) => !pre)}
+                >
+                  Sign Up
+                </button>
               </div>
-              <div>
-                <Button text="BOOK NOW" />
-                {/* <button className=" bg-transparent border-2 text-white border-white px-5 py-3 font-semibold text-[13px] tracking-[2px]">BOOK NOW</button> */}
+            </div>
+
+            <div className="hidden lg:flex min-w-full items-center absolute z-50 text-black px-[40px] xl:px-[90px] justify-between top-20">
+              <Link to="/">
+                <img src={logo} alt="" className="w-[147px] h-[37px] mb-3" />
+              </Link>
+
+              <div
+                onMouseLeave={() => setOffsetLeft(0)}
+                className=" relative flex gap-10 text-white tracking-widest text-[13px] font-semibold "
+              >
+                <div
+                  style={{ transform: `translateX(${offsetLeft}px)` }}
+                  className=" absolute mt-[30px] transition duration-300 w-[18px] h-[2px] bg-white"
+                ></div>
+                <NavLink
+                  to={"/"}
+                  className=" pb-1"
+                  onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
+                >
+                  HOME
+                </NavLink>
+                <div className="">
+                  <NavLink
+                    className=" pb-6"
+                    to={`/`}
+                    onMouseEnter={(e) => {
+                      setIsDropdownOpen(true);
+                      setOffsetLeft(e.target.offsetLeft);
+                    }}
+                    onMouseLeave={() => setIsDropdownOpen(false)}
+                  >
+                    PAGES
+                  </NavLink>
+                  {/* dropdown  */}
+                  <div
+                    onMouseEnter={() => {
+                      setIsDropdownOpen(true);
+                    }}
+                    onMouseLeave={() => {
+                      setTimeout(() => {
+                        setIsDropdownOpen(false);
+                      }, 500);
+                    }}
+                    className={`${
+                      isDropdownOpen ? "block" : "hidden"
+                    } cursor-default absolute top-10 transition duration-500 bg-white w-52 shadow py-5 px-4 flex flex-col space-y-5 z-[100]`}
+                  >
+                    <NavLink
+                      to={"/about-us"}
+                      className="text-[#757575] font-normal text-[16px] transition-all hover:text-black"
+                    >
+                      About Us
+                    </NavLink>
+                    <NavLink
+                      to={"/our-team"}
+                      className="text-[#757575] font-normal text-[16px] transition-all hover:text-black"
+                    >
+                      Our Team
+                    </NavLink>
+                    <NavLink
+                      to={"/hotel-review"}
+                      className="text-[#757575] font-normal text-[16px] transition-all hover:text-black"
+                    >
+                      Hotel Reviews
+                    </NavLink>
+                  </div>
+                </div>
+                <NavLink
+                  to={"/rooms"}
+                  className=" pb-1"
+                  onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
+                >
+                  ROOMS
+                </NavLink>
+                <NavLink
+                  to={"/reservation"}
+                  className=" pb-1"
+                  onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
+                >
+                  RESERVATION
+                </NavLink>
+                <NavLink
+                  to={"/blogs"}
+                  className=" pb-1"
+                  onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
+                >
+                  BLOG
+                </NavLink>
+                <NavLink
+                  to={"/contact"}
+                  className=" pb-1"
+                  onMouseEnter={(e) => setOffsetLeft(e.target.offsetLeft)}
+                >
+                  CONTACT
+                </NavLink>
+              </div>
+              <div className="flex items-center gap-8 text-white">
+                <div className=" flex items-center gap-2">
+                  <div className="">USD</div>
+                  <IoMdArrowDropdown />
+                </div>
+                <div>
+                  <Button text="BOOK NOW" />
+                  {/* <button className=" bg-transparent border-2 text-white border-white px-5 py-3 font-semibold text-[13px] tracking-[2px]">BOOK NOW</button> */}
+                </div>
               </div>
             </div>
           </div>
+
           <div className=" custom-gradient_top h-[344px] top-0 absolute w-full  rounded-[20px]"></div>
           <div className=" custom-gradient_bottom h-[338px] absolute bottom-0  w-full rounded-[20px]"></div>
 
-          <div className="absolute w-full h-full">
-            <div className=" flex flex-col justify-center lg:justify-end items-center h-[50%]">
-              <motion.h1 initial={{opacity:0, y: 100}} animate={{opacity:1, y: 0, rotate: -4}} transition={{duration:1}} className=" font-allison text-[80px] md:text-[126px] text-center leading-[100px] pb-[30px] text-[#FFF] -rotate-[4deg]">
-                Book Your Vacation
-              </motion.h1>
-              <motion.p initial={{opacity:0, y: 100}} animate={{opacity:1, y: 0}} transition={{duration:0.5, delay:1}} className=" text-center text-[#FFFFFF] text-[22px] px-[20px] pb-[30px] mt-0 lg:mt-10 mb-[20px]">
-                Explore new experience with Hotale
-              </motion.p>
-            </div>
+          <div className="  w-full text-center min-h-[400px] md:min-h-[706px] md:pt-[280px] pb-[80px]">
+            <div className=" max-w-[1320px] mx-auto flex flex-col items-center justify-center  px-[20px]">
+              <div className="  flex flex-col items-center justify-center">
+                <motion.h1
+                  initial={{ opacity: 0, y: 100 }}
+                  animate={{ opacity: 1, y: 0, rotate: -4 }}
+                  transition={{ duration: 1 }}
+                  className=" font-allison  text-[70px] md:text-[126px] text-center leading-[100px] pb-[30px] text-[#FFF] -rotate-[4deg]"
+                >
+                  Book Your Vacation
+                </motion.h1>
+                <motion.p
+                  initial={{ opacity: 0, y: 100 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, delay: 1 }}
+                  className=" text-center text-[#FFFFFF] text-[22px] px-[20px] pb-[30px] mt-0 lg:mt-10 mb-[20px]"
+                >
+                  Explore new experience with Hotale
+                </motion.p>
+              </div>
 
-            <div className=" absolute w-full h-[50%] bottom-10  flex justify-center items-start md:items-end md:bottom-40 lg:bottom-20">
-              <form className=" flex flex-col xl:flex-row gap-3 flex-wrap">
+              <form className=" absolute bottom-3 md:bottom-28 flex flex-col xl:flex-row gap-3 flex-wrap">
                 <div>
                   <div className="md:flex gap-[10px]">
                     <div className=" md:w-1/2 bg-white py-[15px] px-[25px] rounded-md mb-3 md:mb-0">
